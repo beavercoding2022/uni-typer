85ee91ab700de4dfc2d655725d711ddba77daa3a
orlein <pcj9024@gmail.com>
2024-06-06 18:54:07 +0900

    remove subpath

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://beavercoding2022.github.io/uni-typer",
+  url: "https://beavercoding2022.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/uni-typer/",
