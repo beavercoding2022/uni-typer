b89e57bd53d07d3cd0468651283f97c10c2159b9
orlein <pcj9024@gmail.com>
2024-07-01 17:34:09 +0900

    fix d.ts type for detailed `decompose` function

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import { FullDecomposed } from "@/decompose.mjs";
 import { Josa } from "@/hangul.mjs";
 
 declare module "@beavercoding/uni-typer" {
-  export function decompose(input: string): string[];
+  export function decompose(input: string): FullDecomposed[];
   export function decompose(input: string, mode: "simple"): string[];
   export function decompose(input: string, mode: "detail"): FullDecomposed[];
   export function combine(input: string[]): string;
